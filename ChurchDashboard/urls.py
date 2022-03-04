f34e34b8628cb08466ba8ac468be685c95478bf7
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-03-04 14:05:17 +0000

    attendances summaries views

--- ChurchDashboard/urls.py
+++ ChurchDashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from ChurchDashboard.views import  list_view_member, list_view_attendance
+from ChurchDashboard.views import  list_view_member, list_view_attendance,attendance_summmaries_list
 
 urlpatterns = [
     path('',list_view_member, name='List_Member'),
     path('attendances/', list_view_attendance, name = 'List_Attendance'),
+    path('att_summ/',attendance_summmaries_list, name = 'Summary_attendance'),
 
 
 ]
--- ChurchDashboard/views.py
+++ ChurchDashboard/views.py
@@ -18,3 +18,11 @@ def  list_view_attendance(request):
 
     }
     return render(request, 'attendance/list.html', context)
+
+
+def attendance_summmaries_list(request):
+    att_summary = AttendanceSummaries.objects.all().order_by('-created_at')
+    context = {
+        'att_summary':att_summary
+    }
+    return render(request, 'attendance/attendance_summary.html', context)
