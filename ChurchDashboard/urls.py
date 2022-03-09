273e5e34653809e9dd45b6fa8c728c4b4a5c3a07
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-03-09 17:10:48 +0000

    corrected the urls of memeber

--- ChurchDashboard/urls.py
+++ ChurchDashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ChurchDashboard.views import  *
 urlpatterns = [
     path('',index_page, name ='home_page'),
-    path('/members',list_view_member, name='list_member_url'),
+    path('members/',list_view_member, name='list_member_url'),
     path('attendances/', list_view_attendance, name = 'list_attendance_url'),
     path('att_summ/',attendance_summmaries_list, name = 'list_summary_attendance_url'),
     path('groups/',groups_list, name = 'list_group_url'),
