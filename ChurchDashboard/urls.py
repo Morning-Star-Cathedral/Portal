fe605362c632ae072aa9d1f306e05adf7891e930
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-03-09 17:08:38 +0000

    added an index and a count of members, groups, DBusers and Chapels

--- ChurchDashboard/urls.py
+++ ChurchDashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from ChurchDashboard.views import  *
 urlpatterns = [
-    path('',list_view_member, name='list_member_url'),
+    path('',index_page, name ='home_page'),
+    path('/members',list_view_member, name='list_member_url'),
     path('attendances/', list_view_attendance, name = 'list_attendance_url'),
     path('att_summ/',attendance_summmaries_list, name = 'list_summary_attendance_url'),
     path('groups/',groups_list, name = 'list_group_url'),
