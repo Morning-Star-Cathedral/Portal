35572266a41379c9d9b62587c369793046548620
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-03-25 15:49:09 +0000

    Admin section

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import *
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 # Create your views here.
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
