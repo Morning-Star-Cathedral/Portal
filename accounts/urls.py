4f6f99b3aa0227b4af6e5128b921883eccd32da8
FiifiQwontwo <quophi555@gmail.com>
2022-05-06 18:18:15 +0000

    Login page not working

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', loginPage, name='login_user'),
     path('logout/', LogoutUser, name='logout'),
-    path('signup/', register_user, name='sign_in'),
+    path('newdance/', register_user, name='sign_in'),
     path('update/', edit_user, name='update_url'),
 
 ]
